32a4280ac8f63850aab15231f48d860de9151ce7
MayraMacedo <38172586+MayraMacedo@users.noreply.github.com>
2018-05-25 11:35:13 -0500

    Update Lista_enlazada.cpp

--- Lista_enlazada.cpp
+++ Lista_enlazada.cpp
@@ -1,89 +1,216 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+template<typename T> class Lista;
+template<typename T>
 class Nodo
 {
+	friend class Lista<T>;
+	T valor;
+	Nodo<T>*next;
 	public:
-		int dato;
-		Nodo *next;
-		Nodo():dato(0), next(NULL){}
-		Nodo(int d): dato(d), next(NULL){}
-		Nodo(Nodo* lista): dato(lista->dato),next(lista->next){};
+		Nodo(T &datos):valor(datos), next(0){};
+		T getValor(){return valor;};
 };
-class Lista_en : public Nodo
-{
-	public:
-		Nodo* xs;
-		Lista_en(): xs(NULL){}
-		Lista_en(int valor)
-		{
-			Nodo *n=new Nodo(valor);
-        	xs=n;	
-		}
-	
-		void Insertar(int valor)
-		{
-			Nodo *nuevo_nodo= new Nodo(valor);
-			Nodo* aux1;
-			Nodo* aux2;
-			aux1=xs;
-			
-			if (xs==aux1)
-				xs=nuevo_nodo;
-			else
-				aux2->next=nuevo_nodo;
-			nuevo_nodo->next=aux1;
-		}
-		void mostrar()
-		{
-			while(xs!=NULL)
-			{
-				cout << xs->dato<<endl;
-				xs=xs->next;
-			}
-		}
 
-		void eliminar()
-		{
-			if (xs==NULL)
-				cout<<"No hay elementos..."<<endl;
-			else
-			{
-				Nodo* anterior;
-				Nodo* posterior=xs;
-				Nodo* temp;
-				int valor;
-				mostrar();
-				cout<<"Elemento a eliminar: "; cin>>valor;
-				while(posterior!=NULL)
-				{
-					if (posterior->dato== valor)
-					{
-						temp=posterior->next;
-						delete posterior;
-						anterior->next=temp;
-						xs=anterior;
-						break;						
-					}
-					else
-					{
-						anterior=posterior	;
-						posterior=posterior->next;
-						
-					}		
-				}
-				mostrar();
-			}
-		}		
+
+template<typename T>
+class Lista
+{
+	Nodo<T> *first;
+	Nodo<T> *last;
+	Nodo<T> *nuevoNodo(T &);	
+	public:
+		Lista();
+		~Lista();
+		void pushFrente(T &);
+		void pushFinal(T &);
+		bool popFrente( T &);
+		bool popFinal(T &);
+		bool isEmpty();
+		void print();
 };
+
+template<typename T>
+Lista <T>::Lista() 
+{
+	first=0;
+	last=0;
+}
+
+template<typename T>
+Lista <T>:: ~Lista()
+{
+	if(!isEmpty())
+	{
+		Nodo <T>*actual=first;
+		Nodo<T>*temp;
+		while(actual!= 0)
+		{
+			temp=actual;
+			cout<<temp->valor<<endl;
+			actual=actual->next;
+			delete temp;
+		}
+	}
+}
+
+template <typename T>
+void Lista<T>:: pushFrente(T &dato)
+{
+	Nodo<T>*nuevo = nuevoNodo(dato);
+	if (isEmpty())
+		first = last = nuevo;
+	else
+	{
+		nuevo->next=first;
+		first=nuevo;
+	}
+		
+}
+template <typename T>
+void Lista<T>:: pushFinal(T &dato)
+{
+	Nodo< T>*nuevo = nuevoNodo(dato);
+	if(isEmpty())
+		first=last=nuevo;
+	else
+	{
+		last->next=nuevo;
+		last=nuevo;
+	}
+		
+}
+
+template <typename  T>
+bool Lista<T>::popFrente(T &dato)
+{
+	if(isEmpty())
+		return false;
+	else
+	{
+		Nodo <T> *temp =first;
+		if(first==last)
+			first=last=0;
+		else
+		{
+			first=first->next;
+		}
+			
+		dato = temp->valor;
+		delete temp;
+		return true;
+		
+	}
+		
+}
+template <typename  T>
+bool Lista<T>::popFinal(T &dato)
+{
+	if(isEmpty())
+		return false;
+	else
+	{
+		Nodo <T> *temp=last;
+		if(first==last)
+			first=last=0;
+		else
+		{
+		
+			Nodo <T>*actual=first;
+			while(actual->next != last)
+				actual=actual->next;
+				
+		last=actual;
+		actual->next=0;
+		}
+		dato=temp->valor;
+		delete temp;
+		return true;			
+	}
+}
+template <typename  T>
+bool Lista<T>::isEmpty()
+{
+	return first==0;
+}
+
+template <typename  T>
+Nodo<T>*Lista<T>::nuevoNodo(T &dato)
+{
+	return new Nodo<T>(dato);
+}
+template <typename  T>
+void Lista<T>::print()
+{
+	if (isEmpty())
+	{
+		cout<<"\n Lista Vacia"<<endl;
+		return;
+	}
+		
+	Nodo<T>*actual=first;
+	cout<<"La lista es: ";
+	while(actual!=0)
+	{
+		cout<< actual->valor<<" ";
+		actual=actual->next;
+	}
+	cout<<endl;
+}
+void instrucciones()
+{
+	cout<<"Ingrese una opcion: "<<endl;
+	cout<<" 1 pushFrente"<<endl;
+	cout<<" 2 pushFinal"<<endl;
+	cout<<" 3 popFrente"<<endl;
+	cout<<" 4 popFinal"<<endl;
+	cout<<" 5 salir"<<endl;
+}
+
+template<typename T>
+void comprobar(Lista <T> &o, string &type)
+{
+	instrucciones();
+	int op;
+	T valor;
+	do
+	{	cout<<"\n ? ";
+		cin>>op; 
+		switch(op)
+		{
+			case 1:
+				cout<< "Escriba tipo de dato "<<type<<": "; cin>>valor;
+				o.pushFrente(valor);
+				o.print();
+				break;
+			case 2:
+				cout<< "Escriba tipo de dato "<<type<<": "; cin>>valor;
+				o.pushFinal(valor);
+				o.print();
+				break;
+			case 3:
+				if (o.popFrente(valor))
+					cout<< valor <<" se elimino de la lista"<<endl;
+				o.print();
+				break;
+			case 4:
+				if (o.popFinal(valor))
+					cout<< valor <<" se elimino de la lista"<<endl;
+				o.print();								
+		}
+	}while(op!=5);
+}
+
 
 int main()
 {
-	Lista_en l;
-	l.Insertar(3);
-	l.Insertar(4);
-	l.Insertar(5);
-	l.eliminar();
-	cout <<"\n"<< "ok";
+	
+	Lista<int> xsEnteros;
+	string s="entero";
+	comprobar(xsEnteros,s);
+
+
 	return 0;
 }
